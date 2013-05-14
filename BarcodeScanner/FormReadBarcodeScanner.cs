cd20a10d262eafea5c2cfb1a0aaaba5046856ff7
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-05-14 09:05:39 +0330

    * shoroe proje
    * ezafe kardan DLL haye SharpDX
    * ezafe kardan WinForm baraye read kardan barcode

---
+++ BarcodeScanner/FormReadBarcodeScanner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.Multimedia;
+using SharpDX.RawInput;
+
+namespace BarcodeScanner
+{
+    public partial class FormReadBarcodeScanner : Form
+    {
+        public FormReadBarcodeScanner()
+        {
+            InitializeComponent();
+        }
+
+        private void FormReadBarcodeScanner_Load(object sender, EventArgs e)
+        {
+            Device.RegisterDevice(UsagePage.Generic, UsageId.GenericKeyboard, DeviceFlags.None);
+            Device.KeyboardInput += Device_KeyboardInput;
+        }
+
+        void Device_KeyboardInput(object sender, KeyboardInputEventArgs e)
+        {
+            
+        }
+    }
+}
