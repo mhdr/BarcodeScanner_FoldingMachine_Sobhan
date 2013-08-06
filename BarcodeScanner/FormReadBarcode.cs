d0e70dddddeac5fd0a1536e49ae282f82d2f749d
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-08-06 16:18:16 +0330

    fekr mikonam moshkel marbot be HID hal shode bashe,roo system khodam ok shod,ama hanooz ro PC tolid test nakaradam

--- BarcodeScanner/FormReadBarcode.cs
+++ BarcodeScanner/FormReadBarcode.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BarcodeScanner.Lib;
+using NationalInstruments.NetworkVariable;
 using SharpDX.Multimedia;
 using SharpDX.RawInput;
 using SharpDX;
@@ -53,29 +55,40 @@ namespace BarcodeScanner
 
         public bool Device1HIDBeingSet = false;
         public bool Device2HIDBegingSet = false;
-
+        private string deviceName1;
+        private string deviceName2;
+        private List<DeviceInfo> devices;
         private void FormReadBarcode_Load(object sender, EventArgs e)
         {
+            devices = Device.GetDevices();
+            string path = @"C:\HIDs.txt";
+            StreamWriter writer = new StreamWriter(path);
+
+            foreach (var deviceInfo in devices)
+            {
+                writer.WriteLine(deviceInfo.DeviceName);
+            }
+
+            writer.Flush();
+
+            DatabaseEntities entities = new DatabaseEntities();
+            deviceName1 = entities.HIDs.FirstOrDefault(x => x.HIDId == 1).HID1;
+            deviceName2 = entities.HIDs.FirstOrDefault(x => x.HIDId == 2).HID1;
             Device.RegisterDevice(UsagePage.Generic, UsageId.GenericKeyboard, DeviceFlags.None);
             Device.KeyboardInput += Device_KeyboardInput;
         }
 
         void Device_KeyboardInput(object sender, KeyboardInputEventArgs e)
         {
-            var devices = Device.GetDevices();
-
             var device = devices.Find(x => x.Handle == e.Device);
+            int length = device.DeviceName.Length;
 
-            DatabaseEntities entities=new DatabaseEntities();
-
-
-            string deviceName1 = entities.HIDs.FirstOrDefault(x => x.HIDId == 1).HID1;
-            string deviceName2 = entities.HIDs.FirstOrDefault(x => x.HIDId == 2).HID1;
+            string deviceNameAfterReplace = device.DeviceName.Substring(0, length - 1);
 
             // "\\\\?\\HID#VID_0000&PID_0001#6&64975e5&0&0000#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}\0"
 
             //if (device.DeviceName == "\\\\?\\HID#VID_0000&PID_0001#6&164bc7c4&0&0000#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}\0")
-            if (device.DeviceName==deviceName1)
+            if (deviceNameAfterReplace == deviceName1)
             {
                 if (e.State == KeyState.KeyUp)
                 {
@@ -92,7 +105,7 @@ namespace BarcodeScanner
                 }
             }
             //else if (device.DeviceName == "\\\\?\\HID#VID_0000&PID_0001#6&64975e5&0&0000#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}\0")
-            else if (device.DeviceName==deviceName2)
+            else if (deviceNameAfterReplace == deviceName2)
             {
                 if (e.State == KeyState.KeyUp)
                 {
@@ -109,5 +122,6 @@ namespace BarcodeScanner
                 }
             }
         }
+
     }
 }
