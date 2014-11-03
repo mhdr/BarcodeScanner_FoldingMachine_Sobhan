35d2405dcf167fcce6c3dc5b4da4d312dcbe1b73
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2014-11-03 13:57:24 +0330

    * backup hazf shod
    * UserManual.pdf be proje ezafe shod

---
+++ BarcodeScanner/WindowDocumentation.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BarcodeScanner
+{
+    /// <summary>
+    /// Interaction logic for WindowDocumentation.xaml
+    /// </summary>
+    public partial class WindowDocumentation : Window
+    {
+        public WindowDocumentation()
+        {
+            InitializeComponent();
+        }
+    }
+}
